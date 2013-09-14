16159dd612b9ae2de0e0fcdd47799603b4b105f3
Po-Ying Chen <poying.me@gmail.com>
2013-09-14 15:20:36 +0800

    ignore

---
+++ templates/migration.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // logic for transforming into the new state
+  },
+
+  down: function(migration, DataTypes, done) {
+    // logic for reverting the changes
+  }
+};
